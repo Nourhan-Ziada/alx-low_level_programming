a0621f02931e39595a3194c1f24e104d24a5228a
Nour <90463182+Nourhan-Ziada@users.noreply.github.com>
2023-03-16 23:26:25 +0200

    betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main is entry point
- * Descripation for fn: prints "with proper grammar, but the outcome is a piece of art," and put end line
- * return 0 end of program
+ * main - print string
+ * Descripation: print using printf
+ * Return: 0 
  */
 
 int main(void)
 {
         printf("%s","with proper grammar, but the outcome is a piece of art,\n");
-        return(0);
+        return (0);
 }
