dd283e8ca14a4bf4c52dc90e20f933884f692c4c
Nourhan <norsayed248.com>
2023-03-16 14:01:15 -0700

    all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main is entry point
+ * Descripation for fn: prints "\"Programming is like building a multilingual puzzle" 
+ * return 0 end of program 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main is entry point
+ * Descripation for fn: prints "with proper grammar, but the outcome is a piece of art," and put end line
+ * return 0 end of program
+ */
+
+int main(void)
+{
+        printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+        return(0);
+}
